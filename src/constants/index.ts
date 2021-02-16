4b2a9f7be38b5beddcfbc6096c6a5a55182843d7
dimakal <dimakal94@mail.ru>
2021-02-16 17:37:57 +0300

    feat: add bombs

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,3 @@
-export const MAX_COLS = 9
-export const MAX_ROWS = 9
+export const MAX_COLS = 9 // count of columns
+export const MAX_ROWS = 9 // count of rows
+export const NUMBER_OF_BOMBS = 10
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,18 +1,34 @@
-import {MAX_COLS, MAX_ROWS} from "../constants";
+import {MAX_COLS, MAX_ROWS, NUMBER_OF_BOMBS} from "../constants";
 import {Cell, CellState, CellValue} from "../types";
 
 export const generateCells = (): Cell[][] => {
-    const cells: Cell[][] = []
+    let cells: Cell[][] = []
 
+    // generating all cells
     for (let row = 0; row < MAX_ROWS; row++) {
         cells.push([])
         for (let col = 0; col < MAX_COLS; col++) {
-            cells[row]. push({
+            cells[row].push({
                 value: CellValue.none,
                 state: CellState.open
             })
         }
     }
+
+    // randomly put 10 bombs
+    let bombsPlaced = 0
+    while (bombsPlaced < NUMBER_OF_BOMBS) {
+        const randomRow = Math.floor(Math.random() * MAX_ROWS)
+        const randomColumn = Math.floor(Math.random() * MAX_COLS)
+        const currentCell = cells[randomRow][randomColumn]
+        if (currentCell.value !== CellValue.bomb) {
+            cells[randomRow][randomColumn] = {
+                ...cells[randomColumn][randomRow], value: CellValue.bomb
+            }
+        }
+        bombsPlaced++
+    }
+
 
     return cells
 }
